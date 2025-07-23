01bf597173a397fd0169782aecd70adea7e233d0
Drian-0-0 <cristianalves1400@gmail.com>
2025-07-22 23:23:40 -0300

    Update config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ const CONFIG = {
             nextBtn: "Next 💖"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on Junho 14th, 2025? 💖", // The big question!
+            text: "Will you be my Valentine on Julho 23th, 2025? 💖", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
